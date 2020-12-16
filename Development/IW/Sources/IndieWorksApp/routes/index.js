715effebd5d2d1c01f0fe4e56e4e0484a5eb0a6a
piotr1221 <64753803+piotr1221@users.noreply.github.com>
2020-12-16 00:32:59 -0500

    Avance codigo backend

---
+++ Development/IW/Sources/IndieWorksApp/routes/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const router = express.Router()
+const proyectoController = require("../controller/proyectocontroller")
+
+
+
+module.exports = function() {
+
+    
+    return router
+}
